8ab0a7400981df0aeb7b8bf92b1b60605be2fef2
fitrahidayaat <fitrahidayaat@gmail.com>
2024-12-09 18:52:53 +0700

    remove updateRating

--- src/routes/reviewRoutes.ts
+++ src/routes/reviewRoutes.ts
@@ -1,12 +1,11 @@
 import { Router } from "express";
 import { authMiddleware } from "../middlewares/authMiddleware";
 import { upload } from "../middlewares/multerMiddleware";
-import { getReviews, createReview, updateRating } from "../controllers/reviewController";
+import { getReviews, createReview } from "../controllers/reviewController";
 
 const reviewRoutes = Router();
 
 reviewRoutes.get("/destination/:destinationId", getReviews); 
 reviewRoutes.post("/", authMiddleware, upload.single('reviewPhoto'), createReview);
-reviewRoutes.get("/updateRating", updateRating);
 
 export default reviewRoutes;
